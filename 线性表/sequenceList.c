9189290c36a12e74567dfe74d369f13db7347acc
xu.hang <929175050@qq.com>
2019-06-10 18:12:51 +0800

    /

--- 线性表/sequenceList.c
+++ 线性表/sequenceList.c
@@ -1,10 +1,69 @@
 #include <stdio.h>
 #include "./sequenceList.h"
 
+/*
+*  不应该直接使用L.elem[i]去取值
+*  直接取值的话 封装的函数便没有了意义 而且混合使用容易出错
+*  封装的函数里面 很多都没有真正删除元素 只是将长度简短了而已
+*/
+void forEachCallBack(int item, int index) {
+    printf("item: %d  ", item);
+    printf("index: %d\n", index);
+}
+
 int main(){
+    int deleteElement; // 被删除的元素
+    int popElement; // 被pop的元素
+    int shiftElement; // 被shift的元素
+    int getItem;
     sqList L;
+
     // 初始化
     initList(&L);
+    // 检查是否为空
+    isEmpty(L) ? printf("不是空的\n") : printf("是空的\n");
+    // 插入元素
+    listInsert(&L, 0, 1);
+    listInsert(&L, 1, 2);
+    listInsert(&L, 0, 3);
+    listInsert(&L, 3, 4);
+    listInsert(&L, 1, 5);
+
+    // push操作
+    push(&L, 6);
+    // unshift操作
+    unshift(&L, 7);
+    // 7351246
+
+    // 删除操作
+    deleteListElement(&L, 2, &deleteElement);
+    printf("删除的元素是：%d\n", deleteElement);
+    // 731246
+
+    // pop操作
+    pop(&L, &popElement);
+    printf("被pop的元素是：%d\n", popElement);
+    // 73124
+    
+    // shift操作
+    shift(&L, &shiftElement);
+    printf("被shift的元素是：%d\n", shiftElement);
+    // 3124
+
+    // 清空
+    clearList(&L);
+    push(&L, 6);
+    // 6
+    
+    // destoryList(&L);
+    
+    // 获取某个值
+    getListItem(&L, 0, &getItem);
+    printf("获取到的值：%d\n", getItem);
+    // forEach
+    forEach(&L, forEachCallBack);
+    
+
 
     return 0;
 }
--- 线性表/sequenceList.h
+++ 线性表/sequenceList.h
@@ -1,4 +1,4 @@
-#include <stdlib.h>
+#include <stdlib.h> // malloc realloc
 
 #ifndef SEQUENCE_LIST_H
 #define SEQUENCE_LIST_H
@@ -9,9 +9,9 @@
 
 // 顺序列表结构体
 typedef struct {
-    int *firstNode; // 第一个节点地址
-    int currentLength; // 当前长度
-    int currentSize; // 当前存储容量
+    int *elem; // 节点地址 初始是第一个节点的地址
+    int currentLength; // 当前列表长度
+    int currentSize; // 当前存储容量 可用空间
 } sqList;
 
 /*
@@ -20,8 +20,8 @@ typedef struct {
 * @return {int} 状态码 0 成功 1 失败
 */
 int initList(sqList *L) {
-    L -> firstNode = (int*)malloc(LIST_INIT_SIZE * sizeof(int));
-    if(!L -> firstNode) return 1;
+    L -> elem = (int*)malloc(LIST_INIT_SIZE * sizeof(int));
+    if(!L -> elem) return 1;
     L -> currentSize = LIST_INIT_SIZE;
     L -> currentLength = 0;
     return 0;
@@ -36,14 +36,172 @@ int isEmpty(sqList L) {
 }
 
 /*
-* 插入元素
-* 
+* 获取长度
+* @return {int} 列表长度
 */
-int insertBefore(sqList *L, int position, int insertElement) {
-    if(position < 1 || position > L -> currentLength + 1)
+int listLength(sqList L) {
+    return L.currentLength;
 }
 
+/*
+* 插入元素 替换目标位置的元素 后面的元素向后移
+* 
+*/
+int listInsert(sqList *L, int position, int insertElement) {
+    //  1 2 3
+    int *newElem;
+    int *p, *dest;
+    int len = L->currentLength;
+    // 预计位置超出边界
+    if(position < 0 || position > len + 1) {
+        printf("超出边界");
+        return 1;
+    }
 
+    // 内存空间不够 申请空间
+    if(L->currentSize <= L->currentLength) {
+        newElem = (int*)realloc(L->elem, sizeof(int) * (L->currentSize + LISTINCRECEMENT));
+        if(! newElem) {
+            printf("申请空间失败");
+            return 1;
+        }
+        // 新的首地址
+        L->elem = newElem;
+        L->currentSize += LISTINCRECEMENT;
+    }
 
+    // 目标地址 指针加中括号相当于后移
+    dest = &L->elem[position];
 
+    // 指针p初始位置为 列表的最后
+    // 这里的currentLength并没有增加 还没有修改 所以firstNode[L->currentLength]位置是一个空的地址
+    for(p = &L->elem[L->currentLength]; p > dest; p--) {
+        *p = *(p-1); // 互换值
+    }
+    
+    // 插入
+    *dest = insertElement;
+    // 修改长度
+    L->currentLength++;
+    return 0;
+}
+
+/*
+* push
+*/
+int push(sqList *L, int pushElement) {
+    // 列表末尾插入
+    // 0 1 2 _
+    int state = listInsert(L, L->currentLength, pushElement);
+    return state;
+}
+
+/*
+* unshift
+*/
+int unshift(sqList *L, int unshiftElement) {
+    // 列表头部插入
+    int state = listInsert(L, 0, unshiftElement);
+    return state;
+}
+
+/*
+*  删除某个位置的元素 并用变量返回
+*/
+int deleteListElement(sqList *L, int position, int *feedBack) {
+    int *dest; // 目标指针
+    int *last; // 列表末尾指针
+    int *i; // 索引指针
+    if(position < 0 || position > L->currentLength - 1) {
+        printf("删除操作超出边界\n");
+        return 1;
+    }
+    dest = &L->elem[position];
+    last = &L->elem[L->currentLength - 1];
+    // 返回值
+    *feedBack = L->elem[position];
+
+    // 左移元素
+    for(i = dest + 1; i <= last; i++) {
+        *(i-1) = *i;
+    }
+    
+    L->currentLength--;
+    return 0;
+}
+
+/*
+*   pop
+*/
+int pop(sqList *L, int *feedBack) {
+    if( L->currentLength == 0) {
+        printf("列表为空");
+        return 1;
+    }
+    deleteListElement(L, L->currentLength - 1, feedBack);
+    return 0;
+}
+
+/*
+* shift
+*/
+int shift(sqList *L, int *feedBack) {
+    if( L->currentLength == 0) {
+        printf("列表为空");
+        return 1;
+    }
+    deleteListElement(L, 0, feedBack);
+    return 0;
+}
+
+/*
+* forEach 遍历列表
+* @prmas {*function} void(*callBack)(int, int)
+*/
+int forEach(sqList *L, void(*callBack)(int, int)) {
+    int index;
+    if( L->currentLength == 0) {
+        printf("遍历列表为空");
+        return 1;
+    }
+    for(index = 0; index < L->currentLength; index++) {
+        int item = L->elem[index];
+        callBack(item, index);
+    }
+    return 0;
+}
+
+/*
+*  将列表清空 不是销毁
+*
+*/
+void clearList(sqList *L) {
+    L->currentLength = 0;
+}
+
+/*
+*  销毁列表
+*
+*/
+void destoryList(sqList *L) {
+    // 最重要的是回收分配的内存空间
+    // 结构体L的地址 和 元素L.elem的地址不是同一个
+    free(L->elem);
+
+    L->elem = NULL;
+    L->currentSize = 0;
+    L->currentLength = 0;
+}
+
+/*
+* 获取某个位置的元素
+*/
+int getListItem(sqList *L, int position, int *feedBack) {
+    if(position < 0 || position > L->currentLength - 1) {
+        printf("获取元素，超出边界");
+        return 1;
+    }
+    *feedBack = L->elem[position];
+    return 0;
+}
 #endif
